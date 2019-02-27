4983e90d7a2a35c1a94dc59b1c3a981deaa7737f
NIKBAKHT\sarir <hn0297@gmail.com>
2019-02-27 16:56:38 +0330

    creating project asp core 2 adding partials and views to the project upating the bootstrap to the 4.1.3 and the jquery to the 3.3.1 version adding the font awesome version 4.7.3 creating appdbcontext & approles &appusers setting startup &apsetting 1397/12/7

---
+++ Web90TvCore2/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web90TvCore2.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUsers, ApplicationRoles, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+    }
+}
---
+++ Web90TvCore2/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Web90TvCore2.Models;
+
+namespace Web90TvCore2
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // appsetting  تنطیم کردن خواندن از  
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //--------------------------------------------------------
+            //verify Connection String To app
+            services.AddDbContext<ApplicationDbContext>(option =>
+            option.UseSqlServer(Configuration.GetConnectionString("MyConnectionString")));
+
+            //verify Idenity service
+            services.AddIdentity<ApplicationUsers, ApplicationRoles>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders(); //به هرکاربردر هنگام لاگین بودن یک توکن اختصاص میدهد  باتغیرر توکن لاگ وت میشود
+            //-----------------------------------------------------------------------
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                //برای آپدیت کردن کتابخانه های کلاینت مانند بوت استرپ جی کویری و ..
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "node_modules")),
+                    RequestPath = "/" + "node_modules"
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+     
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
