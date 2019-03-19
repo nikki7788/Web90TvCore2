f913119325317e9f17f3cc2e295cb4ad5607195d
NIKBAKHT\sarir <hn0297@gmail.com>
2019-03-19 23:18:28 +0330

    Creating the AccessRight Action(get+post) and its method Adding "checkbox " parameter to Plugins in the jquery of  AccessRight's view for showing checkbox next to each part Adding "e" parameter to the  main function jquery of the accessRight view 1397/12/28 S.33

--- Web90TvCore2/Areas/AdminPanel/Controllers/RoleController.cs
+++ Web90TvCore2/Areas/AdminPanel/Controllers/RoleController.cs
@@ -21,10 +21,12 @@ namespace Web90TvCore2.Areas.AdminPanel.Controllers
         #region ################### dependencies #############################
 
         private readonly RoleManager<ApplicationRoles> _roleManager;
+        private readonly UserManager<ApplicationUsers> _userManager;
 
-        public RoleController(RoleManager<ApplicationRoles> roleManager)
+        public RoleController(RoleManager<ApplicationRoles> roleManager, UserManager<ApplicationUsers> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         #endregion ########################
 
@@ -33,6 +35,7 @@ namespace Web90TvCore2.Areas.AdminPanel.Controllers
         /// نمایش درختواره و جز های آن
         /// </summary>
         /// <returns></returns>
+        [HttpGet]
         public IActionResult Index()
         {
             try
@@ -68,6 +71,11 @@ namespace Web90TvCore2.Areas.AdminPanel.Controllers
                 return View();
             }
             catch (ArgumentNullException ex)
+            {
+
+                throw ex;
+            }
+            catch (NotSupportedException ex)
             {
 
                 throw ex;
@@ -152,5 +160,122 @@ namespace Web90TvCore2.Areas.AdminPanel.Controllers
             }
 
         }
+
+
+
+
+
+        /// <summary>
+        /// Get Method
+        /// نمایش درختواره و جز های آن برای مشخص کردن سطح دسترسی هر کاربر
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult AccessRight()
+        {
+            try
+            {
+                List<TreeViewNode> nodes = new List<TreeViewNode>();
+
+                //جز اصلی و ردیف اول را خودمان به صورت دستی وارد میکنیم
+                nodes.Add(new TreeViewNode
+                {
+                    id = "asd",
+                    parent = "#",   //اولین نود و ردیف پدر ندارد و پدر ان را # میگذاریم
+                    text = "اجزای سیستم"
+                });
+
+                //بقیه ردیف ها ا ازدیتابیس میخواند و از جدول نقش ها
+                foreach (ApplicationRoles subNode in _roleManager.Roles.Where(r => r.RoleLevel != "0"))
+                {
+                    //تمام ردیف ها به جز ردیف اول را از دیتابیس میخواند و برمیگرداند
+
+                    nodes.Add(new TreeViewNode
+                    {
+                        id = subNode.Id.ToString(),
+                        text = subNode.Description,
+                        parent = subNode.RoleLevel.ToString()
+                    });
+
+                }
+
+                // دریافت میکند و ما اطالاعات را تبدیل به جیسان کرده و میفرستیم به ویو json اطلاعات را به صورت jstree
+                ViewBag.json = JsonConvert.SerializeObject(nodes);
+                ViewBag.viewTitle = "  نمایش درختی اجزای سیستم";
+
+                return View();
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                throw ex;
+            }
+            catch (NotSupportedException ex)
+            {
+
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //todo:مدیریت خطاها در لاگمانده
+                throw ex;
+            }
+        }
+
+
+
+        [HttpPost, ActionName("AccessRight")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AccessRightConfirm(string selectedItems, string Id)
+        {
+            // وقتی روی دسترسی هرکاربرکلیک میکنیم ارسال و دراینجا دریافت میکنیم  asp-route-id کاربر را از طریق دستور  Id
+            try
+            {
+                // کردیم deserialize دریافت سطح دسترسی کاربراز ویو که به صورت جیسون است و ان را  
+                //  به صورت ارایه و جیسوندرواقع هرکدام از درختواره ها ک تیک خورده باشد آی دی ان را که همان آی دی نقش است را برمیگرداند
+                List<TreeViewNode> items = JsonConvert.DeserializeObject<List<TreeViewNode>>(selectedItems);
+
+                //کاربر را براساس آی دی دریافتی از ورودی پیدامیکند
+                ApplicationUsers user = await _userManager.FindByIdAsync(Id);
+
+                if (user != null)
+                {
+                    for (int i = 0; i < items.Count; i++)
+                    {
+
+                        ApplicationRoles role = await _roleManager.FindByIdAsync(items[i].id);
+
+                        if (role != null)
+                        {
+                            // اطلاعات را ثبت میکند AspNetUserRoles در جدول 
+                            //آی دی یوزر را به همراه آی دی   نقش ها آن
+                            //یوزر و نام نقش را میگیرد این دستور
+                            IdentityResult result = await _userManager.AddToRoleAsync(user, role.Name);
+                            if (!result.Succeeded)
+                            {
+                                return NotFound();
+                            }
+                        }
+
+                    }
+                    //(action,Controller)
+                    return RedirectToAction("Index", "User");
+                }
+
+                return RedirectToAction("Index", "User"); ;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+
+        }
+
+
+
     }
 }
